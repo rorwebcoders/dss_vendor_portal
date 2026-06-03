051a98ca4556e8faf4fe637eb374cc274112d9fc
Rajeshkumar671 <rajeshkumar@rorwebcoders.com>
2026-06-03 17:43:40 +0530

    Removed migration

--- db/migrate/20260601170008_add_additional_fields_to_dealer_and_po.rb
+++
@@ -1,7 +0,0 @@
-class AddAdditionalFieldsToDealerAndPo < ActiveRecord::Migration[8.0]
-  def change
-    add_column :dealers, :sm_dealer_id, :integer
-    add_column :purchase_orders, :tracking_number, :string
-    add_column :purchase_orders, :shipstation_label_url, :string
-  end
-end
