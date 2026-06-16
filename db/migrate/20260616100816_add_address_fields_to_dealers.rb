953d42bbf70362357e32d5c0b701ca96f92d4305
Rajeshkumar671 <rajeshkumar@rorwebcoders.com>
2026-06-16 17:38:18 +0530

    Added Address fields to dealers

---
+++ db/migrate/20260616100816_add_address_fields_to_dealers.rb
@@ -0,0 +1,15 @@
+class AddAddressFieldsToDealers < ActiveRecord::Migration[8.0]
+  def change
+    add_column :dealers, :address_line1, :string
+    add_column :dealers, :address_line2, :string
+    add_column :dealers, :address_line3, :string
+    add_column :dealers, :city_locality, :string
+    add_column :dealers, :state_province, :string
+    add_column :dealers, :postal_code, :string
+    add_column :dealers, :country_code, :string
+    add_column :dealers, :dealership_name, :string
+    add_column :dealers, :phone, :string
+    add_column :dealers, :shipstation_carrier_codes, :json
+    add_column :dealers, :shipstation_warehouse_id, :string
+  end
+end
