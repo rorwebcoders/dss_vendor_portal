aceaa104bf03d21a7911e98069523a5d183259b6
Rajeshkumar671 <rajeshkumar@rorwebcoders.com>
2026-06-17 00:10:07 +0530

    Listed ServiceCodes in Admin#Carrier

--- app/admin/carriers.rb
+++ app/admin/carriers.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Carrier do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :shipstation_carrier_id, :shipstation_carrier_code, :shipstation_friendly_name, :shipstation_account_number, :enabled
+  permit_params :shipstation_carrier_id, :shipstation_carrier_code, :shipstation_friendly_name, :shipstation_account_number, :enabled,service_codes_attributes: [:id, :shipstation_service_code, :shipstation_name, :domestic, :international, :_destroy]
   #
   # or
   #
@@ -14,5 +14,48 @@ ActiveAdmin.register Carrier do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+  actions :index, :show
+
+  show do
+    attributes_table do
+      row :id
+      row :shipstation_carrier_id
+      row :shipstation_carrier_code 
+      row :shipstation_friendly_name 
+      row :shipstation_account_number 
+      row :enabled 
+      row :created_at
+      row :updated_at
+    end
+
+    panel "Service Codes" do
+      table_for carrier.service_codes.order(:id) do
+        column :shipstation_service_code
+        column :shipstation_name
+        column :domestic
+        column :international
+        column :created_at
+      end
+    end
+  end
+
+  # form do |f|
+  #   f.inputs "Carrier Details" do
+  #     f.input :shipstation_carrier_id, input_html: { disabled: true, readonly: true }
+  #     f.input :shipstation_carrier_code, input_html: { disabled: true, readonly: true }
+  #     f.input :shipstation_friendly_name, input_html: { disabled: true, readonly: true }
+  #     f.input :shipstation_account_number, input_html: { disabled: true, readonly: true }
+  #     f.input :enabled, input_html: { disabled: true, readonly: true }
+  #   end
+  #   f.inputs "Service Codes" do
+  #     f.has_many :service_codes, allow_destroy: false, new_record: false do |sc|
+  #       sc.input :shipstation_service_code, input_html: { disabled: true, readonly: true }
+  #       sc.input :shipstation_name, input_html: { disabled: true, readonly: true }
+  #       sc.input :domestic, input_html: { disabled: true, readonly: true }
+  #       sc.input :international, input_html: { disabled: true, readonly: true }
+  #     end
+  #   end
+
+  #   f.actions
+  # end
 end
--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -1,3 +1,4 @@
 class Carrier < ApplicationRecord
   has_many :service_codes, dependent: :destroy
+  accepts_nested_attributes_for :service_codes, allow_destroy: true
 end
